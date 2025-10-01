73bcbe1aea94d29a0f07d1ad36ce25a71a8d7924
md <chelov134999@gmail.com>
2025-10-01 19:45:37 +0800

    Update star-engine LIFF form with hooking report

---
+++ star-engine-form/config.js
@@ -0,0 +1,6 @@
+window.STAR_ENGINE_CONFIG = {
+  liffId: '2008215846-5LwXlWVN',
+  webhookUrl: 'https://chelov134999.app.n8n.cloud/webhook/lead-entry',
+  googlePlacesApiKey: 'AIzaSyCRy2IFDTjfA0co5EBggpPTcZG323YPaNY',
+  scraperApiKey: '5b62aa6ae725da5b1162a4674a3ea49e'
+};
